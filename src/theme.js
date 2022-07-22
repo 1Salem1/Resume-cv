e5628cbae46497374a744e929dd845345f72454c
salem <salem.dahmani345@gmail.com>
2022-07-22 16:58:36 +0200

    'v1'

---
+++ src/theme.js
@@ -0,0 +1,39 @@
+// Theme Presets
+
+const lightTheme = {
+  name: "light",
+  body: "#FFFFFF",
+  text: "#343434",
+  dark: "#F0AE2E",
+  secondaryText: "#7F8DAA",
+  accentColor: "#E3405F",
+  accentBright: "#FC1056",
+  projectCard: "#DCE4F2",
+  skinColor: "#914900",
+  skinColor2: "#914900",
+  imageDark: "#dce4f2",
+  imageClothes: "#276DC6",
+  avatarMisc: "#e9ecf2",
+  avatarShoes: "#ccd2e3",
+  hair : '#E39203'
+};
+
+const darkTheme = {
+  name: "dark",
+  body: "#1D1D1D",
+  text: "#FFFFFF",
+  dark: "#F0AE2E",
+  secondaryText: "#8D8D8D",
+  accentColor: "#E3405F",
+  accentBright: "#FC1056",
+  projectCard: "#292A2D",
+  skinColor: "#914900",
+  skinColor2: "#914900",
+  imageDark: "#292A2D",
+  imageClothes: "#276DC6",
+  avatarMisc: "#212121",
+  avatarShoes: "#2B2B2B",
+  hair : '#E39203'
+};
+
+export const themes = { light: lightTheme, dark: darkTheme };
